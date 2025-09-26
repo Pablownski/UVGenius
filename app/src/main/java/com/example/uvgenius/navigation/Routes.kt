f9d8dcc76f2db4e43db960adc96125e835704dd5
Pablownski <sal24374@uvg.edu.gt>
2025-09-25 19:15:08 -0600

    gitignore e implementacion de navegacion

---
+++ app/src/main/java/com/example/uvgenius/navigation/Routes.kt
@@ -0,0 +1,9 @@
+package com.example.uvgenius.navigation
+
+object Routes {
+    const val LOGIN = "login"
+    const val HOME = "home"
+    const val TutorDetail = "tutor_detail"
+}
+
+
---
+++ app/src/main/java/com/example/uvgenius/ui/screens/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.uvgenius.ui.screens
+
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(rootNav: NavHostController, vm: AppVM){
+
+}
