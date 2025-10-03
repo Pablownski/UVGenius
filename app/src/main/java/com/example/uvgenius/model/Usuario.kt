83a9f8fa899ab7413400e871ec1a424c9d6d8303
Pablownski <sal24374@uvg.edu.gt>
2025-10-02 20:36:54 -0600

    Implementacion clase Usuario.kt

---
+++ app/src/main/java/com/example/uvgenius/model/Usuario.kt
@@ -0,0 +1,15 @@
+package com.example.uvgenius.model
+
+
+
+import androidx.annotation.DrawableRes
+
+data class Usuario(
+    val nombre: String,
+    val password: String,
+    val carrera: String,
+    var cursos: List<String>,
+    val telefono: String,
+    val email: String,
+    @DrawableRes val avatar: Int
+)
